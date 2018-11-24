cd094477953fe58422c814478aa3da3f2a3401d7
Hung Dang <hdang@athenahealth.com>
2018-11-24 01:05:50 -0500

    Init regex.cpp

---
+++ unittests/regex.cpp
@@ -0,0 +1,19 @@
+#include <cstring>
+#include "fmt/format.h"
+
+namespace index_engine {
+    class RegexToTrie {
+    public:
+
+        void operator()(const char * pattern) {
+            
+        }
+        
+    private:
+        
+    };
+}
+
+int main() {
+    
+}
